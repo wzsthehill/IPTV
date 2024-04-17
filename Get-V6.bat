54016610339f68ac1fb8e0938b1564e359ff7321
wzsthehill <wzsthehill@gmail.com>
2024-04-17 13:31:11 +0800

    20240417-1330

--- Get-V6.bat
+++ Get-V6.bat
@@ -1,4 +1,3 @@
 wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.m3u" -O "IPV6-1.m3u"
-wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/fenxp/iptv/main/live/iptv.txt" -O "IPV6-1.txt"
 wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/drangjchen/IPTV/main/M3U/ipv6.m3u" -O "IPV6-2.m3u"
 wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/BurningC4/Chinese-IPTV/master/TV-IPV4.m3u" -O "IPV6-3.m3u"
