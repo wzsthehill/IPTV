379ac37f574e7774b6dff815991101c88d7cbf63
wzsthehill <wzsthehill@gmail.com>
2024-07-15 02:19:33 +0800

    2024-07-15 02:19

--- Get-V4.bat
+++ Get-V4.bat
@@ -1,2 +1,2 @@
-wget "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.m3u" -O "IPV4-1.m3u"
-wget "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt" -O "IPV4-1.txt"
+wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/ssili126/tv/main/itvlist.m3u" -O "IPV4-1.m3u"
+wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt" -O "IPV4-1.txt"
