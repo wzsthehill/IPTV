e572feaf692bc33124e5c591f05540ed7ee9c853
wzsthehill <wzsthehill@gmail.com>
2024-07-15 02:24:27 +0800

    2024-07-15 02:19

--- Get-V6.bat
+++ Get-V6.bat
@@ -1,4 +1,4 @@
-wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/ngo5/IPTV/main/m3u/ipv6Plus.m3u" -O "ipv6Plus.m3u"
+wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/ngo5/IPTV/main/m3u/ipv6Plus.m3u" -O "IPV6-Plus.m3u"
 wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u" -O "IPV6.m3u"
 wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.m3u" -O "IPV6-1.m3u"
 wget "https://mirror.ghproxy.com/https://raw.githubusercontent.com/drangjchen/IPTV/main/M3U/ipv6.m3u" -O "IPV6-2.m3u"
